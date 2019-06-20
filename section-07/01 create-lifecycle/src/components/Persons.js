4f0ff49ce20e35f5d970d9edf70aed4eb09e094d
xgirma <xgirma@gmail.com>
2019-06-19 23:20:58 -0700

    create life cycle

---
+++ section-07/01 create-lifecycle/src/components/Persons.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Person from './Person/Person';
+
+const persons = props => {
+  console.log('[Persons.js] render');
+  
+  return props.persons.map((person, index) => {
+    return <Person
+      key={person.id}
+      click={() => props.deletePerson(index)}
+      name={person.name}
+      age={person.age}
+      changed={(event) => props.nameChange(event, person.id)}
+    />
+  })};
+
+export default persons;
