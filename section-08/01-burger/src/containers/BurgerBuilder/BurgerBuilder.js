5e58ce1ddb80f54286e7317152b54a9a06dd5006
xgirma <xgirma@gmail.com>
2019-06-25 19:16:32 -0700

    moved builder to container

--- section-08/01-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ section-08/01-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,8 +1,8 @@
 import React, {Component, Fragment} from 'react';
-import Burger from './../Burger/Burger';
-import BuildControls from './../Burger/BuildControls/BuildControls';
-import Modal from './../UI/Modal/Modal';
-import OrderSummary from '../Burger/OrderSummary/OrderSummary';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
